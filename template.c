414db7b7421ac5e21735ee2a3794e1717128e5db
Furkan Cayci <furkanca02@gmail.com>
2019-01-30 22:03:49 +0300

    spaces

--- template.c
+++ template.c
@@ -45,40 +45,40 @@
  *   page 267 from rm0090
  */
 typedef struct {
-	uint32_t CR;         /* Address offset: 0x00 */
-	uint32_t PLLCFGR;    /* Address offset: 0x04 */
-	uint32_t CFGR;       /* Address offset: 0x08 */
-	uint32_t CIR;        /* Address offset: 0x0C */
-	uint32_t AHB1RSTR;   /* Address offset: 0x10 */
-	uint32_t AHB2RSTR;   /* Address offset: 0x14 */
-	uint32_t AHB3RSTR;   /* Address offset: 0x18 */
-	uint32_t RESERVED1;  /* Address offset: 0x1C */
-	uint32_t APB1RSTR;   /* Address offset: 0x20 */
-	uint32_t APB2RSTR;   /* Address offset: 0x24 */
-	uint32_t RESERVED2;  /* Address offset: 0x28 */
-	uint32_t RESERVED3;  /* Address offset: 0x2C */
-	uint32_t AHB1ENR;    /* Address offset: 0x30 */
-	uint32_t AHB2ENR;    /* Address offset: 0x34 */
-	uint32_t AHB3ENR;    /* Address offset: 0x38 */
-	uint32_t RESERVED4;  /* Address offset: 0x3C */
-	uint32_t APB1ENR;    /* Address offset: 0x40 */
-	uint32_t APB2ENR;    /* Address offset: 0x44 */
-	uint32_t RESERVED5;  /* Address offset: 0x48 */
-	uint32_t RESERVED6;  /* Address offset: 0x4C */
-	uint32_t AHB1LPENR;  /* Address offset: 0x50 */
-	uint32_t AHB2LPENR;  /* Address offset: 0x54 */
-	uint32_t AHB3LPENR;  /* Address offset: 0x58 */
-	uint32_t RESERVED7;  /* Address offset: 0x5C */
-	uint32_t APB1LPENR;  /* Address offset: 0x60 */
-	uint32_t APB2LPENR;  /* Address offset: 0x64 */
-	uint32_t RESERVED8;  /* Address offset: 0x68 */
-	uint32_t RESERVED9;  /* Address offset: 0x6C */
-	uint32_t BDCR;       /* Address offset: 0x70 */
-	uint32_t CSR;        /* Address offset: 0x74 */
-	uint32_t RESERVED10; /* Address offset: 0x78 */
-	uint32_t RESERVED11; /* Address offset: 0x7C */
-	uint32_t SSCGR;      /* Address offset: 0x80 */
-	uint32_t PLLI2SCFGR; /* Address offset: 0x84 */
+    uint32_t CR;         /* Address offset: 0x00 */
+    uint32_t PLLCFGR;    /* Address offset: 0x04 */
+    uint32_t CFGR;       /* Address offset: 0x08 */
+    uint32_t CIR;        /* Address offset: 0x0C */
+    uint32_t AHB1RSTR;   /* Address offset: 0x10 */
+    uint32_t AHB2RSTR;   /* Address offset: 0x14 */
+    uint32_t AHB3RSTR;   /* Address offset: 0x18 */
+    uint32_t RESERVED1;  /* Address offset: 0x1C */
+    uint32_t APB1RSTR;   /* Address offset: 0x20 */
+    uint32_t APB2RSTR;   /* Address offset: 0x24 */
+    uint32_t RESERVED2;  /* Address offset: 0x28 */
+    uint32_t RESERVED3;  /* Address offset: 0x2C */
+    uint32_t AHB1ENR;    /* Address offset: 0x30 */
+    uint32_t AHB2ENR;    /* Address offset: 0x34 */
+    uint32_t AHB3ENR;    /* Address offset: 0x38 */
+    uint32_t RESERVED4;  /* Address offset: 0x3C */
+    uint32_t APB1ENR;    /* Address offset: 0x40 */
+    uint32_t APB2ENR;    /* Address offset: 0x44 */
+    uint32_t RESERVED5;  /* Address offset: 0x48 */
+    uint32_t RESERVED6;  /* Address offset: 0x4C */
+    uint32_t AHB1LPENR;  /* Address offset: 0x50 */
+    uint32_t AHB2LPENR;  /* Address offset: 0x54 */
+    uint32_t AHB3LPENR;  /* Address offset: 0x58 */
+    uint32_t RESERVED7;  /* Address offset: 0x5C */
+    uint32_t APB1LPENR;  /* Address offset: 0x60 */
+    uint32_t APB2LPENR;  /* Address offset: 0x64 */
+    uint32_t RESERVED8;  /* Address offset: 0x68 */
+    uint32_t RESERVED9;  /* Address offset: 0x6C */
+    uint32_t BDCR;       /* Address offset: 0x70 */
+    uint32_t CSR;        /* Address offset: 0x74 */
+    uint32_t RESERVED10; /* Address offset: 0x78 */
+    uint32_t RESERVED11; /* Address offset: 0x7C */
+    uint32_t SSCGR;      /* Address offset: 0x80 */
+    uint32_t PLLI2SCFGR; /* Address offset: 0x84 */
 } RCC_type;
 
  /*
@@ -86,16 +86,16 @@ typedef struct {
   *   page 288 from rm0090
   */
 typedef struct {
-	uint32_t MODER;      /* Address offset: 0x00 */
-	uint32_t OTYPER;     /* Address offset: 0x04 */
-	uint32_t OSPEEDR;    /* Address offset: 0x08 */
-	uint32_t PUPDR;      /* Address offset: 0x0C */
-	uint32_t IDR;        /* Address offset: 0x10 */
-	uint32_t ODR;        /* Address offset: 0x14 */
-	uint32_t BSRR;       /* Address offset: 0x18 */
-	uint32_t LCKR;       /* Address offset: 0x1C */
-	uint32_t AFRL;       /* Address offset: 0x20 */
-	uint32_t AFRH;       /* Address offset: 0x24 */
+    uint32_t MODER;      /* Address offset: 0x00 */
+    uint32_t OTYPER;     /* Address offset: 0x04 */
+    uint32_t OSPEEDR;    /* Address offset: 0x08 */
+    uint32_t PUPDR;      /* Address offset: 0x0C */
+    uint32_t IDR;        /* Address offset: 0x10 */
+    uint32_t ODR;        /* Address offset: 0x14 */
+    uint32_t BSRR;       /* Address offset: 0x18 */
+    uint32_t LCKR;       /* Address offset: 0x1C */
+    uint32_t AFRL;       /* Address offset: 0x20 */
+    uint32_t AFRH;       /* Address offset: 0x24 */
 } GPIO_type;
 
 // Create pointers based at the given addresses
@@ -126,22 +126,22 @@ extern unsigned long __stack;
 // For STM32F407 - Vector table can be found on page 374 in RM0090
 __attribute__ ((section(".vectors")))
 void (* const vector_table[])(void) = {
-	(intfunc)((unsigned long)&__stack),	/* 0x000 Stack Pointer */
-	Reset_Handler,               		/* 0x004 Reset         */
-	Default_Handler,					/* 0x008 NMI           */
-	Default_Handler,                    /* 0x00C HardFaullt    */
-	Default_Handler,                    /* 0x010 MemManage     */
-	Default_Handler,                    /* 0x014 BusFault      */
-	Default_Handler,                    /* 0x018 UsageFault    */
-	0,                   			    /* 0x01C Reserved      */
-	0,     				                /* 0x020 Reserved      */
-	0,               			        /* 0x024 Reserved      */
-	0, 				                    /* 0x028 Reserved      */
-	Default_Handler,                    /* 0x02C SVCall        */
-	Default_Handler,                    /* 0x030 Debug Monitor */
-	0,                                  /* 0x034 Reserved      */
-	Default_Handler,                    /* 0x038 PendSV        */
-	Default_Handler                     /* 0x03C SysTick       */
+    (intfunc)((unsigned long)&__stack),	/* 0x000 Stack Pointer */
+    Reset_Handler,               		/* 0x004 Reset         */
+    Default_Handler,					/* 0x008 NMI           */
+    Default_Handler,                    /* 0x00C HardFaullt    */
+    Default_Handler,                    /* 0x010 MemManage     */
+    Default_Handler,                    /* 0x014 BusFault      */
+    Default_Handler,                    /* 0x018 UsageFault    */
+    0,                   			    /* 0x01C Reserved      */
+    0,     				                /* 0x020 Reserved      */
+    0,               			        /* 0x024 Reserved      */
+    0, 				                    /* 0x028 Reserved      */
+    Default_Handler,                    /* 0x02C SVCall        */
+    Default_Handler,                    /* 0x030 Debug Monitor */
+    0,                                  /* 0x034 Reserved      */
+    Default_Handler,                    /* 0x038 PendSV        */
+    Default_Handler                     /* 0x03C SysTick       */
 };
 
 /*
@@ -153,32 +153,32 @@ void (* const vector_table[])(void) = {
  */
 void Reset_Handler(void)
 {
-	/* initialize data and bss sections */
-	_init_data();
+    /* initialize data and bss sections */
+    _init_data();
 
-	// /* Reset the RCC clock configuration to the default reset state */
-  	// /* Set HSION bit */
-  	// RCC->CR |= (1 << 0);
+    // /* Reset the RCC clock configuration to the default reset state */
+      // /* Set HSION bit */
+      // RCC->CR |= (1 << 0);
 
-	// /* Reset CFGR register */
-	// RCC->CFGR = 0x00000000;
+    // /* Reset CFGR register */
+    // RCC->CFGR = 0x00000000;
 
-	// /* Reset HSEON (16), CSSON (19) and PLLON (24) bits */
-	// RCC->CR &= ~(uint32_t)((1 << 16) | (1 << 19) | (1 << 24));
+    // /* Reset HSEON (16), CSSON (19) and PLLON (24) bits */
+    // RCC->CR &= ~(uint32_t)((1 << 16) | (1 << 19) | (1 << 24));
 
-	// /* Reset PLLCFGR register to reset value */
-	// RCC->PLLCFGR = 0x24003010;
+    // /* Reset PLLCFGR register to reset value */
+    // RCC->PLLCFGR = 0x24003010;
 
-	// /* Reset HSEBYP bit */
-	// RCC->CR &= ~(uint32_t)(1 << 18);
+    // /* Reset HSEBYP bit */
+    // RCC->CR &= ~(uint32_t)(1 << 18);
 
-	// /* Disable all interrupts */
-	// RCC->CIR = 0x00000000;
+    // /* Disable all interrupts */
+    // RCC->CIR = 0x00000000;
 
-	// main functions
-	main();
-	// Wait forever
-	for (;;);
+    // main functions
+    main();
+    // Wait forever
+    for (;;);
 }
 
 /*************************************************
@@ -187,23 +187,23 @@ void Reset_Handler(void)
 *************************************************/
 void _init_data(void)
 {
-	extern unsigned long __etext, __data_start__, __data_end__, __bss_start__, __bss_end__;
-	unsigned long *src = &__etext;
-	unsigned long *dst = &__data_start__;
+    extern unsigned long __etext, __data_start__, __data_end__, __bss_start__, __bss_end__;
+    unsigned long *src = &__etext;
+    unsigned long *dst = &__data_start__;
 
-	/* ROM has data at end of text; copy it.  */
-	while (dst < &__data_end__)
-		*dst++ = *src++;
+    /* ROM has data at end of text; copy it.  */
+    while (dst < &__data_end__)
+        *dst++ = *src++;
 
-	/* zero bss.  */
-	for (dst = &__bss_start__; dst< &__bss_end__; dst++)
-		*dst = 0;
+    /* zero bss.  */
+    for (dst = &__bss_start__; dst< &__bss_end__; dst++)
+        *dst = 0;
 }
 
 // Default handler function
 void Default_Handler(void)
 {
-	for (;;);  // Wait forever
+    for (;;);  // Wait forever
 }
 
 // initialized data
@@ -218,58 +218,58 @@ const uint32_t carray[4] = {1, 4, 1, 4};
 *************************************************/
 int32_t main(void)
 {
-	// dummy copy. doesn't actually do anything
-	// added to show where the variables are placed
-	for(int i=0; i<4; i++){
-		iarray[i] = carray[i];
-		uarray[i] = carray[i];
-	}
+    // dummy copy. doesn't actually do anything
+    // added to show where the variables are placed
+    for(int i=0; i<4; i++){
+        iarray[i] = carray[i];
+        uarray[i] = carray[i];
+    }
 
-	// Each module is powered separately. In order to turn on a module
-	// we need to enable the relevant clock.
-	// Set Bit 3 to enable GPIOD clock in AHB1ENR
-	// AHB1ENR: XXXX XXXX XXXX XXXX XXXX XXXX XXXX 1XXX
-	RCC->AHB1ENR |= 0x00000008;
+    // Each module is powered separately. In order to turn on a module
+    // we need to enable the relevant clock.
+    // Set Bit 3 to enable GPIOD clock in AHB1ENR
+    // AHB1ENR: XXXX XXXX XXXX XXXX XXXX XXXX XXXX 1XXX
+    RCC->AHB1ENR |= 0x00000008;
 
-	// Another way to write a 1 to a bit location is to shift it that much
-	// Meaning shift number 1, 3 times to the left. Which would result in
-	// 0b1000 or 0x8
-	// RCC->AHB1ENR |= (1 << 3);
+    // Another way to write a 1 to a bit location is to shift it that much
+    // Meaning shift number 1, 3 times to the left. Which would result in
+    // 0b1000 or 0x8
+    // RCC->AHB1ENR |= (1 << 3);
 
-	// In order to make a pin output, we need to write 01 to the relevant
-	// section in MODER register
-	// We first need to AND it to reset them, then OR it to set them.
-	//                     bit31                                         bit0
-	// MODER register bits : 01 01 01 01 XX XX XX XX XX XX XX XX XX XX XX XX
-	//                      p15                                           p0
+    // In order to make a pin output, we need to write 01 to the relevant
+    // section in MODER register
+    // We first need to AND it to reset them, then OR it to set them.
+    //                     bit31                                         bit0
+    // MODER register bits : 01 01 01 01 XX XX XX XX XX XX XX XX XX XX XX XX
+    //                      p15                                           p0
 
-	GPIOD->MODER &= 0x00FFFFFF;   // Reset bits 31-24 to clear old values
-	GPIOD->MODER |= 0x55000000;   // Set MODER bits to 01 (0101 is 5 in hex)
+    GPIOD->MODER &= 0x00FFFFFF;   // Reset bits 31-24 to clear old values
+    GPIOD->MODER |= 0x55000000;   // Set MODER bits to 01 (0101 is 5 in hex)
 
-	// You can do the same setup with shifting
-	// GPIOD->MODER &= ~(0xFF << 24); //or GPIOD->MODER &= ~(0b11111111 << 24);
-	// GPIOD->MODER |=  (0x55 << 24); //or GPIOD->MODER |=  (0b01010101 << 24);
+    // You can do the same setup with shifting
+    // GPIOD->MODER &= ~(0xFF << 24); //or GPIOD->MODER &= ~(0b11111111 << 24);
+    // GPIOD->MODER |=  (0x55 << 24); //or GPIOD->MODER |=  (0b01010101 << 24);
 
-	// Set Pins 12-15 to 1 to turn on all LEDs
-	// ODR: 1111 XXXX XXXX XXXX
-	GPIOD->ODR |= 0xF000;
+    // Set Pins 12-15 to 1 to turn on all LEDs
+    // ODR: 1111 XXXX XXXX XXXX
+    GPIOD->ODR |= 0xF000;
 
-	// You can do the same setup with shifting
-	// GPIOD->ODR |= (0xF << 12);
+    // You can do the same setup with shifting
+    // GPIOD->ODR |= (0xF << 12);
 
-	while(1){
-		delay(LEDDELAY);
-		GPIOD->ODR ^= 0xF000;  // Toggle LEDs
-	}
+    while(1){
+        delay(LEDDELAY);
+        GPIOD->ODR ^= 0xF000;  // Toggle LEDs
+    }
 
-	__asm__("NOP"); // Assembly inline can be used if needed
-	return 0;
+    __asm__("NOP"); // Assembly inline can be used if needed
+    return 0;
 }
 
 // A simple and not accurate delay function
 void delay(volatile uint32_t s)
 {
-	for(s; s>0; s--){
-		// Do nothing
-	}
+    for(s; s>0; s--){
+        // Do nothing
+    }
 }
